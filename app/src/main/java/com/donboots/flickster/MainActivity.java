5172bfab906b2f73f43fe9863c718879d04e52e0
dboots <don@donboots.com>
2016-07-24 19:21:29 -0700

    formatting and pull to refresh

--- app/src/main/java/com/donboots/flickster/MainActivity.java
+++ app/src/main/java/com/donboots/flickster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.donboots.flickster;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,25 +21,28 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeContainer;
     ArrayList<Movie> movies;
     MovieArrayAdapter movieAdapter;
     ListView lvItems;
+    AsyncHttpClient client;
+    JsonHttpResponseHandler movieResponseHandler;
+    String movieUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         lvItems = (ListView) findViewById(R.id.lvMovies);
         movies = new ArrayList<>();
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        movieUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler() {
-
+        movieResponseHandler = new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray results = null;
@@ -58,6 +62,22 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
             }
+        };
+
+        client = new AsyncHttpClient();
+        client.get(movieUrl, movieResponseHandler);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                movieAdapter.clear();
+                client.get(movieUrl, movieResponseHandler);
+                swipeContainer.setRefreshing(false);
+            }
         });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_blue_dark);
     }
+
+
 }
